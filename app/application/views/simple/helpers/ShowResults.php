7874fc63e433a63f8c947921b09df3b090e34ca2
Zefiryn <zefiryn@jewula.net>
2014-02-25 22:13:25 +0100

    fix check result date for links in menu

--- app/application/views/simple/helpers/ShowResults.php
+++ app/application/views/simple/helpers/ShowResults.php
@@ -5,7 +5,7 @@ class Helper_ShowResults extends Zend_View_Helper_Abstract
 	{
 		$appSettings = Zend_Registry::get('appSettings');
 		 
-		if ($appSettings->application_deadline <= time() || $this->view->user->_role == 'admin')
+		if ($appSettings->result_date <= time() || $this->view->user->_role == 'admin')
 		{
 			return TRUE;
 		}
